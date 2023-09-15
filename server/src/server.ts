9b719857a7b5b82db76c031f8118112fb90c84fb
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-15 00:55:57 -0300

    running-server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,8 @@ import { fastify } from 'fastify'
 import cors from '@fastify/cors'
 import { promptRoutes } from './routes/prompt'
 import { uploadRoutes } from './routes/upload'
+import { completionRoutes } from './routes/completion'
+import { transcriptionRoutes } from './routes/transcription'
 
 const app = fastify()
 
@@ -11,7 +13,9 @@ app.register(cors, {
 
 app.register(promptRoutes)
 app.register(uploadRoutes)
+app.register(completionRoutes)
+app.register(transcriptionRoutes)
 
 app
-  .listen({ port: 3333, host: '0.0.0.0' })
+  .listen({ port: 3333 })
   .then(() => console.log('server running...'))
